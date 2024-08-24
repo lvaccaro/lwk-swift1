d43c76c348c1ea4686a2564decc578910b692b90
Luca Vaccaro <me@lvaccaro.com>
2024-08-24 18:36:04 +0200

    build5
    Signed-off-by: Luca Vaccaro <me@lvaccaro.com>

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .library(name: "Lwk", targets: ["lwkFFI", "Lwk"]),
     ],
     targets: [
-        .binaryTarget(name: "lwkFFI", url: "https://github.com/lvaccaro/lwk-swift1/releases/download/test5/lwk.xcframework.zip", checksum: "801f54f654f11df352c91d8d550d66e4fbe1aeb38814a1697f5bce40e8bb243c"),
+        .binaryTarget(name: "lwkFFI", url: "https://github.com/lvaccaro/lwk-swift1/releases/download/test6/lwk.xcframework.zip", checksum: "cef4755a3d35c83010ad3f0ce3b293a02e0f0d438fa0cb19c343eb51ec84c08b"),
         .target(name: "Lwk", dependencies: ["lwkFFI"]),
     ]
 )
